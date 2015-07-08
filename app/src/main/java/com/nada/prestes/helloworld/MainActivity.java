ac4bda7af7575f6f61c5d35d21501b91365472e7
ftprestes <ftprestes@gmail.com>
2015-07-08 16:59:53 -0300

    Versão 2

--- app/src/main/java/com/nada/prestes/helloworld/MainActivity.java
+++ app/src/main/java/com/nada/prestes/helloworld/MainActivity.java
@@ -2,7 +2,11 @@ package com.nada.prestes.helloworld;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,64 +16,51 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
+
 /**
  * Created by prestes on 04/07/15.
  */
 public class MainActivity extends Activity{
 
-    MediaPlayer triangleSound0, triangleSound1, triangleSound2;
-    int p = 1;
     private TextView text;
     private SeekBar seekBar;
     private Button btnPlay, btnStop;
+    private SoundPool triangleSound = null;
+    private float j = 1.0f;
+    private int sID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        triangleSound1 = MediaPlayer.create(MainActivity.this, R.drawable.music_triangulo);
-        triangleSound0 = MediaPlayer.create(MainActivity.this, R.drawable.teste0);
-        triangleSound2 = MediaPlayer.create(MainActivity.this, R.drawable.teste2);
         btnPlay = (Button)findViewById(R.id.buttonPlay);
         btnStop = (Button)findViewById(R.id.buttonStop);
         seekBar = (SeekBar)findViewById(R.id.speedChoose);
         seekBar.setVisibility(View.INVISIBLE);
         text = (TextView) findViewById(R.id.textView);
+        triangleSound = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
 
         btnPlay.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                triangleSound1.start();
-                triangleSound1.setLooping(true);
+                sID = triangleSound.load(MainActivity.this, R.drawable.music_triangulo, 1);
+                triangleSound.play(sID, 1, 1, 1, -1, 1);
+
+
                 seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
                     @Override
                     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                        p = progress;
-                        text.setText(Integer.toString(p) + "X");
-                        if (p == 0) {
-                            if (triangleSound1.isPlaying())
-                                triangleSound1.pause();
-                            if (triangleSound2.isPlaying())
-                                triangleSound2.pause();
-                            triangleSound0.start();
-                            triangleSound0.setLooping(true);
+
+                        text.setText(Integer.toString(progress) + "X");
+                        if (progress == 0) {
+
                         }
-                        if (p == 1) {
-                            if (triangleSound0.isPlaying())
-                                triangleSound0.pause();
-                            if (triangleSound2.isPlaying())
-                                triangleSound2.pause();
-                            triangleSound1.start();
-                            triangleSound1.setLooping(true);
+                        if (progress == 1) {
+
                         }
-                        if (p == 2) {
-                            if (triangleSound1.isPlaying())
-                                triangleSound1.pause();
-                            if (triangleSound0.isPlaying())
-                                triangleSound0.pause();
-                            triangleSound2.start();
-                            triangleSound2.setLooping(true);
+                        if (progress == 2) {
                         }
                     }
 
@@ -80,8 +71,8 @@ public class MainActivity extends Activity{
                     @Override
                     public void onStopTrackingTouch(SeekBar seekBar) {
                     }
+
                 });
-                seekBar.setProgress(1);
                 seekBar.setVisibility(View.VISIBLE);
                 btnPlay.setVisibility(View.INVISIBLE);
                 btnStop.setVisibility(View.VISIBLE);
@@ -91,13 +82,8 @@ public class MainActivity extends Activity{
         btnStop.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                seekBar.setProgress(1);
                 text.setText(Integer.toString(1) + "X");
-                if(triangleSound1.isPlaying())
-                    triangleSound1.pause();
-                if(triangleSound0.isPlaying())
-                    triangleSound0.pause();
-                if(triangleSound2.isPlaying())
-                    triangleSound2.pause();
                 seekBar.setVisibility(View.INVISIBLE);
                 btnStop.setVisibility(View.INVISIBLE);
                 btnPlay.setVisibility(View.VISIBLE);
@@ -109,24 +95,15 @@ public class MainActivity extends Activity{
     @Override
     protected void onStop() {
         super.onStop();
-        triangleSound1.release();
-        triangleSound0.release();
-        triangleSound2.release();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        triangleSound1.release();
-        triangleSound0.release();
-        triangleSound2.release();
     }
 
     @Override
    protected void onPause() {
        super.onPause();
-        triangleSound1.release();
-        triangleSound0.release();
-        triangleSound2.release();
    }
 }
